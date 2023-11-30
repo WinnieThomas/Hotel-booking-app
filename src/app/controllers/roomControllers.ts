920597d40df171e6cacb93d77ab648550ba1b9f7
WinnieThomas <winnieantony1988@gmail.com>
2023-11-30 22:23:29 +0530

    added update room

--- src/app/controllers/roomControllers.ts
+++ src/app/controllers/roomControllers.ts
@@ -2,9 +2,14 @@ import Room from "@/models/room";
 import { NextRequest, NextResponse } from "next/server";
 
 export const allRooms = async(req:NextRequest) => {
+
+  const resPerPage:number = 8;
+  const rooms = await Room.find();
+
   return NextResponse.json({
-    data:"hello",
-   });
+    success:true,
+    rooms,
+  })
 
 }
 
@@ -17,4 +22,52 @@ export const newRoom = async (req:NextRequest) =>{
     success:true,
     room,
   })
+}
+
+export const getRoomDetails = async (req:NextRequest, {params}:{params:{id:string}}) => {
+
+  const room = await Room.findById(params.id);
+
+  if(!room){
+    return NextResponse.json({
+      message:"room not found!",
+    },
+    {
+      status:404,
+    }
+    );
+  }
+
+  return NextResponse.json({
+    success:true,
+    room,
+  });
+
+}
+
+
+export const updateRoom = async (req:NextRequest, {params}:{params:{id:string}}) => {
+
+  let room = await Room.findById(params.id);
+  const body = await req.json();
+
+  if(!room){
+    return NextResponse.json({
+      message:"room not found!",
+    },
+    {
+      status:404,
+    }
+    );
+  }
+
+  room= await Room.findByIdAndUpdate(params.id, body {
+    new:true,
+  });
+
+  return NextResponse.json({
+    success:true,
+    room,
+  });
+
 }
