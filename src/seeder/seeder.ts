26e68c5d5b52cb29df5882a17d5f5b4485a5cf17
WinnieThomas <winnieantony1988@gmail.com>
2023-12-07 07:26:18 +0530

    added seeder

---
+++ src/seeder/seeder.ts
@@ -0,0 +1,23 @@
+import Room from "../models/room";
+import mongoose from "mongoose";
+import { rooms } from "./data";
+
+
+const seedRooms = async() =>{
+    try{
+
+        await mongoose.connect("mongodb://127.0.0.1:27017/bookit-v2");
+        await Room.deleteMany();
+        console.log("Rooms are deleted");
+        await Room.insertMany(rooms);
+        console.log("Rooms are added");
+        process.exit();
+
+    }
+    catch(error){
+        console.log(error);
+        process.exit();
+    }
+}
+
+seedRooms();
